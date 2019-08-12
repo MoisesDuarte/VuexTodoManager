f6d4b42b1ea765fc5d74aa4ab2ca1b28d6585e8e
MoisesDuarte <moisesduartejunior@gmail.com>
2019-08-12 11:26:18 -0300

    Criando componenete AddTodos.vue, Definindo actions e mutations para Adição de Todo em todos.js

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -15,11 +15,17 @@ const actions = {
         const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
 
         commit('setTodos', response.data); // Chamando mutation setTodos com commit
+    },
+    async addTodo({ commit }, title) {
+        const response = await axios.post('https://jsonplaceholder.typicode.com/todos', { title, completed: false });
+
+        commit('newTodo', response.data)
     }
 };
 
 const mutations = {
-    setTodos: (state, todos) => (state.todos = todos) // Todos que são passados pela função são passados para o array todos em state
+    setTodos: (state, todos) => (state.todos = todos), // Todos que são passados pela função são passados para o array todos em state
+    newTodo: (state, todo) => state.todos.unshift(todo)
 };
 
 export default {
